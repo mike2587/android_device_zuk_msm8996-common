bdf9b8ac363f61805b8ffe8c0c4fa71526341297
Artem Borisov <dedsa2002@gmail.com>
2018-12-24 15:25:51 +0400

    msm8996-common: Add QCOM's WFD implementation
    * Sadly, out-of-the-box Miracast support was killed in P, so
      we need this to keep Wi-Fi display feature working.
    
    Change-Id: Id680d918b64c0e3733251db45992ec477b6fbdfb

--- msm8996.mk
+++ msm8996.mk
@@ -397,7 +397,15 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-	
-#Zuk Extras
+
+# WiFi Display
 PRODUCT_PACKAGES += \
-    ZukPref	
+    libaacwrapper \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+	
+# Zuk Extras
+PRODUCT_PACKAGES += \
+    ZukPref
